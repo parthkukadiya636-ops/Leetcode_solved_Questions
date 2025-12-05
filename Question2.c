77752b28d8de149abd8387a01fbca774214015b5
Parth kukadiya <parthkukadiya636@gmail.com>
2025-12-05 23:11:27 +0530

    Leetcode

---
+++ Question2.c
@@ -0,0 +1,12 @@
+int checkPerfectNumber(int num) {
+    if(num == 6 || num == 28 || num == 496 || 
+           num == 8128 || num == 33550336){
+            return 1;
+           }
+    else{
+        return 0;
+    }       
+}
+
+
+// note  remember this number which are only perfect square which are sum of all the number whom they are divisible with.
