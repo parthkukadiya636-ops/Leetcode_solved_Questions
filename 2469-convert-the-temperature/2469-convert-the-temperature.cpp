68bc76c4881e238c2235788caa7c99f38bd8e93e
Parth kukadiya <parthkukadiya636@gmail.com>
2026-07-31 21:38:42 +0530

    Time: 0 ms (100%), Space: 8.2 MB (96.93%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+
+        double kelvin;
+        double fahren;
+        vector<double>ans;
+
+         
+
+        kelvin = celsius + 273.15;
+        ans.push_back(kelvin);
+        fahren = celsius * 1.80 + 32.00;
+        ans.push_back(fahren);
+
+        return ans;
+
+    }
+};
